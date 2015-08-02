0a6f48c9fce184642dc0eac6950e3f23acdc0c87
Colby Brown <cbrownlinux@gmail.com>
2015-08-01 21:22:44 -0500

    Set up customization base

--- web/js/shop-controllers.min.js
+++ web/js/shop-controllers.min.js
@@ -28,6 +28,35 @@ app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
 			url: '/home',
 			templateUrl: 'home.html',
 			controller: 'HomeController'
+		})
+		.state('base.customize', {
+			url: '/customize',
+			templateUrl: 'customize.html'
+		})
+		.state('base.customize.backings', {
+			url: '/backings',
+			parent: 'base.customize',
+			template: 'Backings'
+		})
+		.state('base.customize.accentbows', {
+			url: '/accentbows',
+			parent: 'base.customize',
+			template: 'Accent Bows'
+		})
+		.state('base.customize.ribbons', {
+			url: '/ribbons',
+			parent: 'base.customize',
+			template: 'Name Ribbons'
+		})
+		.state('base.customize.bears', {
+			url: '/bears',
+			parent: 'base.customize',
+			template: 'Bears'
+		})
+		.state('base.customize.accessories', {
+			url: '/accessories',
+			parent: 'base.customize',
+			template: 'Accessories'
 		});
 
 		$httpProvider.defaults.post = {'Content-Type': 'application/x-www-form-urlencoded'};
